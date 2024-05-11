570c9816bd7ff5e0e22badc3e47339ed7bc6fe19
jcmullwh <78822128+jcmullwh@users.noreply.github.com>
2024-05-11 12:59:52 -0400

    chore: Update run function to accept TcpListener as parameter and fix import

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use zero2prod_rust::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Bind to a random port provided by the OS
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,23 @@
-use zero2prod_rust::run;
-
-fn spawn_app() {
-    let server = zero2prod_rust::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Get the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod_rust::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    return format!("http://127.0.0.1:{}", port);
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
